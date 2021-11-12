76f9671b4a8f68583c07a491d59f80df376d9e54
morita0209 <79314848+morita0209@users.noreply.github.com>
2021-11-12 22:50:28 +0800

    Add files via upload

---
+++ SpringBoot-Portfolio-20211018/src/main/java/com/example/demo/SpringBootPortfolio20211018Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class SpringBootPortfolio20211018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPortfolio20211018Application.class, args);
+	}
+
+}
